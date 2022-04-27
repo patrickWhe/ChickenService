7175715c59bf72f892bdaf890c5fdf9db3a1ecb2
patrickwhe <A00291195@student.ait.ie>
2022-04-27 23:19:10 +0100

    tsting push

--- src/main/java/com/patrickwhel/inventory/app/chickens/InventoryAppChickensApplication.java
+++ src/main/java/com/patrickwhel/inventory/app/chickens/InventoryAppChickensApplication.java
@@ -3,14 +3,14 @@ package com.patrickwhel.inventory.app.chickens;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.openfeign.EnableFeignClients;
+
 
 
 
 
 @SpringBootApplication
 @EnableDiscoveryClient
-@EnableFeignClients
+
 
 public class InventoryAppChickensApplication {
 
