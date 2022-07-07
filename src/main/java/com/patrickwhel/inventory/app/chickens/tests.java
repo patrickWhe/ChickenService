1de325759827913a6c01e191d96f126602ab2e29
patrickWhe <98049912+patrickWhe@users.noreply.github.com>
2022-07-07 15:30:52 +0100

    Update tests.java

--- src/main/java/com/patrickwhel/inventory/app/chickens/tests.java
+++ src/main/java/com/patrickwhel/inventory/app/chickens/tests.java
@@ -1,9 +1,17 @@
 package com.patrickwhel.inventory.app.chickens.test;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.Before;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -20,11 +28,12 @@ public class TestChickenApp extends InventoryAppChickensApplication.tests {
 	}
 
 	@Test
-	public void testChicken() throws Exception {
+	public void testEmployee() throws Exception {
 		mockMvc.perform(get("/chicken")).andExpect(status().isOk())
 				.andExpect(content().contentType("application/json;charset=UTF-8"))
-				.andExpect(jsonPath("$.name")):
-			
+				.andExpect(jsonPath("$.name"));
+				
+
 	}
 
 }
